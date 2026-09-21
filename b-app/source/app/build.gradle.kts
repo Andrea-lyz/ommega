1959ee1d82d4f97f36f0e9854674e152350f7536
Andrea-lyz <Andrea-lyz@users.noreply.github.com>
2026-09-22 00:37:14 +0200

    chore: release 1.6.0

--- b-app/source/app/build.gradle.kts
+++ b-app/source/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.ommega.deviceb"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.5.3-ommega"
+        versionCode = 26
+        versionName = "1.6.0-ommega"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
